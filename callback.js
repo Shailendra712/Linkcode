d2a635f9d7202c0d19b63babc170bcbdbe649962
Shailendra712 <157387697+Shailendra712@users.noreply.github.com>
2025-03-21 23:14:59 +0530

    Add files via upload

---
+++ callback.js
@@ -0,0 +1,11 @@
+
+function greet(name, callback) {
+    console.log("Hello, " + name);
+    callback(); 
+  }
+  
+  function sayGoodbye() {
+    console.log("Good Morning!");
+  }
+  
+  greet("Vinit", sayGoodbye);
